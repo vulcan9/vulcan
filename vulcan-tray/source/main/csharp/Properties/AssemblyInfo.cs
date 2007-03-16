dad60f21d5bba49d8fadf4d818f5f56a2627afc6
chris.eldredge <chris.eldredge@localhost>
2007-03-16 17:12:03 +0000

    Reorganized code.

---
+++ vulcan-tray/source/main/csharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SourceForge.Vulcan.Tray")]
+[assembly: AssemblyDescription("Notification Tray Application for Vulcan")]
+[assembly: AssemblyProduct("SourceForge.Vulcan.Tray")]
+[assembly: AssemblyCopyright("Copyright (C) 2006")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4aa2c4ef-04c0-4d02-8c06-02741239d3bc")]
