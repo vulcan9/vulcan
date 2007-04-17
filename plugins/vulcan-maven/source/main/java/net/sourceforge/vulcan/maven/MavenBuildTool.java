bd98005ee36591c4ea9dc173bc227a8fe4ce85c8
chris.eldredge <chris.eldredge@localhost>
2007-04-17 00:47:53 +0000

    Fixed regex which incorrectly matched hyphens.

--- plugins/vulcan-maven/source/main/java/net/sourceforge/vulcan/maven/MavenBuildTool.java
+++ plugins/vulcan-maven/source/main/java/net/sourceforge/vulcan/maven/MavenBuildTool.java
@@ -291,7 +291,7 @@ public class MavenBuildTool extends AntBuildTool {
 			eventSource.addSystemProperties(jcb);
 		}
 
-		final String[] goals = mavenProjectConfig.getTargets().split("\\W+");
+		final String[] goals = mavenProjectConfig.getTargets().split("[ \t]+");
 		for (String goal : goals) {
 			jcb.addArgument(goal);
 		}
